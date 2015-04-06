8e4979e783d895dc973e1a6c237faefd6a70866e
iandownie <ian.b.downie@gmail.com>
2015-04-06 15:54:03 -0400

    finished loader

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -5,26 +5,40 @@ app.controller('MainController', function(FlashCardsFactory, $scope, $http) {
 	    'Express',
 	    'Angular',
 	    'Node'];
+	// $scope.getLoader=function(){
 
-	
+	// 	console.log("test")
+	// }
+
+
 	$scope.getCategoryCards=function(cat){
-		FlashCardsFactory.getFlashCards(cat).then(function(data){
-		$scope.flashCards=data;
-
-		$scope.myCategory=cat;
-		console.log($scope.myCategory)
-	});
+			$scope.loader=true;
+			console.log("first: ",$scope)
+			//setTimeout(function(){
+				FlashCardsFactory.getFlashCards(cat).then(function(data){
+					$scope.flashCards=data;
+					$scope.myCategory=cat;
+					$scope.loader=false;
+					console.log("second: ",$scope.loader)
+				});
+			//}, 3000);
+		
 	};
 	$scope.resetCards=function(){
+		$scope.loader=true;
+
 		FlashCardsFactory.getFlashCards().then(function(data){
 		$scope.flashCards=data
 		$scope.myCategory=false;
+		$scope.loader=false;
 	});
 };
 
 
 	FlashCardsFactory.getFlashCards().then(function(data){
 		$scope.flashCards=data
+			//$scope.loader = false;
+
 	});
 // console.log($scope.flashCards);
 	// FlashCardsFactory.getFlashCards().then(function(data){
@@ -32,4 +46,4 @@ app.controller('MainController', function(FlashCardsFactory, $scope, $http) {
 	// });
 	    
 
-});
+	});
